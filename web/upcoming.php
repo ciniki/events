4fe84d303b4d6f765dc26ce326b397870a5dddde
Andrew Rivett <veggiefrog@gmail.com>
2012-04-22 14:21:27 -0400

    Started work on web delivery

---
+++ web/upcoming.php
@@ -0,0 +1,39 @@
+<?php
+//
+// Description
+// -----------
+// This function will return a list of events, with the dates formatted for english suffix
+//
+// Arguments
+// ---------
+// api_key:
+// auth_token:
+// business_id:		The ID of the business to get events for.
+//
+// Returns
+// -------
+// <events>
+// 	<event id="" name="" />
+// </events>
+//
+function ciniki_events_webUpcoming($ciniki, $business_id) {
+
+	$strsql = "SELECT id, name, url, description, "
+		. "DATE_FORMAT(start_date, '%M') AS start_month, "
+		. "DATE_FORMAT(start_date, '%D') AS start_day, "
+		. "DATE_FORMAT(start_date, '%Y') AS start_year, "
+		. "IF(end_date = '0000-00-00', '', DATE_FORMAT(end_date, '%M')) AS end_month, "
+		. "IF(end_date = '0000-00-00', '', DATE_FORMAT(end_date, '%D')) AS end_day, "
+		. "IF(end_date = '0000-00-00', '', DATE_FORMAT(end_date, '%Y')) AS end_year, "
+		. "DATE_FORMAT(start_date, '%b %c, %Y') AS start_date, "
+		. "DATE_FORMAT(end_date, '%b %c, %Y') AS end_date "
+		. "FROM ciniki_events "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND (end_date >= DATE(NOW()) OR start_date >= DATE(NOW())) "
+		. "ORDER BY start_date ASC "
+		. "";
+
+    require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbRspQuery.php');
+	return ciniki_core_dbRspQuery($ciniki, $strsql, 'events', 'events', 'event', array('stat'=>'ok', 'events'=>array()));
+}
+?>
