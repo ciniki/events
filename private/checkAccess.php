25c893ddbcd26945c344b7eb88298ca788f18eb6
Andrew Rivett <veggiefrog@gmail.com>
2013-01-16 19:42:14 -0500

    Fixed bug with checkAccess not looking for active users, signup not assigning proper status to business_user, and lists not working when adding a new painting

--- private/checkAccess.php
+++ private/checkAccess.php
@@ -42,6 +42,7 @@ function ciniki_events_checkAccess($ciniki, $business_id, $method) {
 		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
 		. "AND user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
 		. "AND package = 'ciniki' "
+		. "AND status = 10 "
 		. "AND (permission_group = 'owners' OR permission_group = 'employees') "
 		. "";
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQuery');
