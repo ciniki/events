c69957ef91daaa3d66c9df43f79e3df23de85276
Andrew Rivett <veggiefrog@gmail.com>
2014-01-02 20:29:03 -0500

    Updated loadCacheThumbnail to include business_id

--- public/eventGet.php
+++ public/eventGet.php
@@ -99,7 +99,7 @@ function ciniki_events_eventGet($ciniki) {
 		if( isset($event['images']) ) {
 			foreach($event['images'] as $img_id => $img) {
 				if( isset($img['image']['image_id']) && $img['image']['image_id'] > 0 ) {
-					$rc = ciniki_images_loadCacheThumbnail($ciniki, $img['image']['image_id'], 75);
+					$rc = ciniki_images_loadCacheThumbnail($ciniki, $args['business_id'], $img['image']['image_id'], 75);
 					if( $rc['stat'] != 'ok' ) {
 						return $rc;
 					}
