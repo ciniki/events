a606c459067e1e42f408f5fcad2697179f156e43
Andrew Rivett <veggiefrog@gmail.com>
2014-07-23 20:11:36 -0400

    Updated available_to maps

--- private/maps.php
+++ private/maps.php
@@ -13,11 +13,12 @@
 function ciniki_events_maps($ciniki, $modules) {
 	$maps = array();
 	$maps['prices'] = array('available_to'=>array(
-		0x01=>'Customers',
+		0x01=>'Public',
 		0x02=>'Private',
-		0x10=>'Members',
-		0x20=>'Dealers',
-		0x40=>'Distributors',
+		0x10=>'Customers',
+		0x20=>'Members',
+		0x40=>'Dealers',
+		0x80=>'Distributors',
 		));
 
 	return array('stat'=>'ok', 'maps'=>$maps);
