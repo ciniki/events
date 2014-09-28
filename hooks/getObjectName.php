9693b7d52310824b0e41d81784ad83c0b536632a
Andrew Rivett <veggiefrog@gmail.com>
2014-09-27 23:33:41 -0400

    Added hooks to get object name

---
+++ hooks/getObjectName.php
@@ -0,0 +1,37 @@
+<?php
+//
+// Description
+// -----------
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_events_hooks_getObjectName($ciniki, $business_id, $args) {
+
+	// Set the default to not used
+	$used = 'no';
+	$count = 0;
+	$msg = '';
+
+	if( isset($args['object']) && $args['object'] == 'ciniki.events.event' 
+		&& isset($args['object_id']) && $args['object_id'] != '' ) {
+		$strsql = "SELECT name "
+			. "FROM ciniki_events "
+			. "WHERE id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+			. "AND business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+			. "";
+		$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.events', 'event');
+		if( $rc['stat'] != 'ok' ) {
+			return $rc;
+		}
+		if( isset($rc['event']['name']) ) {
+			return array('stat'=>'ok', 'name'=>$rc['event']['name']);
+		}
+	}
+	
+	return array('stat'=>'noexist', 'err'=>array('pkg'=>'ciniki', 'code'=>'2045', 'msg'=>'Could not find event'));
+}
+?>
