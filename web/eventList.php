fe7034fd87c47061da2e13b2e31c11743eaad879
Andrew Rivett <veggiefrog@gmail.com>
2014-10-27 09:30:30 -0400

    Fixed bug with events showing up in both current and past events

--- web/eventList.php
+++ web/eventList.php
@@ -41,7 +41,7 @@ function ciniki_events_web_eventList($ciniki, $settings, $business_id, $type, $l
 		. "WHERE ciniki_events.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' ";
 	if( $type == 'past' ) {
 		$strsql .= "AND ((ciniki_events.end_date > ciniki_events.start_date AND ciniki_events.end_date < DATE(NOW())) "
-				. "OR (ciniki_events.end_date <= ciniki_events.start_date AND ciniki_events.start_date <= DATE(NOW())) "
+				. "OR (ciniki_events.end_date <= ciniki_events.start_date AND ciniki_events.start_date < DATE(NOW())) "
 				. ") "
 			. "GROUP BY ciniki_events.id "
 			. "ORDER BY ciniki_events.start_date DESC "
