fc5023b4114b2d5a0c8b52e802bbf66aaaad91f6
Andrew Rivett <veggiefrog@gmail.com>
2017-11-19 14:40:29 -0500

    Updated checkObjectUsed hook to check for image usage

--- hooks/checkObjectUsed.php
+++ hooks/checkObjectUsed.php
@@ -11,13 +11,16 @@
 //
 function ciniki_events_hooks_checkObjectUsed($ciniki, $business_id, $args) {
 
-    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbCount');
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbSingleCount');
 
     // Set the default to not used
     $used = 'no';
     $count = 0;
     $msg = '';
 
+    //
+    // Check if customer is used anywhere
+    //
     if( $args['object'] == 'ciniki.customers.customer' ) {
         //
         // Check the invoice customers
@@ -27,7 +30,7 @@ function ciniki_events_hooks_checkObjectUsed($ciniki, $business_id, $args) {
             . "WHERE customer_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
             . "AND business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
             . "";
-        $rc = ciniki_core_dbCount($ciniki, $strsql, 'ciniki.sapos', 'num');
+        $rc = ciniki_core_dbCount($ciniki, $strsql, 'ciniki.events', 'num');
         if( $rc['stat'] != 'ok' ) {
             return $rc;
         }
@@ -35,6 +38,52 @@ function ciniki_events_hooks_checkObjectUsed($ciniki, $business_id, $args) {
             $used = 'yes';
             $count = $rc['num']['items'];
             $msg .= ($msg!=''?' ':'') . "There " . ($count==1?'is':'are') . " $count event registration" . ($count==1?'':'s') . " for this customer.";
+        }
+    }
+
+    //
+    // Check if image is used anywhere
+    //
+    if( $args['object'] == 'ciniki.images.image' ) {
+        //
+        // Check the events
+        //
+        $count = 0;
+        $strsql = "SELECT COUNT(*) AS items "
+            . "FROM ciniki_events "
+            . "WHERE primary_image_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+            . "AND business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+            . "";
+        $rc = ciniki_core_dbSingleCount($ciniki, $strsql, 'ciniki.events', 'num');
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        if( $rc['num'] > 0 ) {
+            $count += $rc['num'];
+        }
+
+        //
+        // Check the event images
+        //
+        $strsql = "SELECT COUNT(*) AS items "
+            . "FROM ciniki_event_images "
+            . "WHERE image_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+            . "AND business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+            . "";
+        $rc = ciniki_core_dbSingleCount($ciniki, $strsql, 'ciniki.events', 'num');
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        if( $rc['num'] > 0 ) {
+            $count += $rc['num'];
+        }
+
+        //
+        // Check if used
+        //
+        if( $count > 0 ) {
+            $used = 'yes';
+            $msg .= ($msg!=''?' ':'') . "There " . ($rc['num']==1?'is':'are') . " {$rc['num']} event" . ($rc['num']==1?'':'s') . " using this image.";
         }
     }
 
