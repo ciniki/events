610351e814e29b24966b6be49d0b78a4c3c4bebf
Andrew Rivett <veggiefrog@gmail.com>
2018-03-31 14:09:48 -0400

    Added hooks for web indexing

--- public/eventAdd.php
+++ public/eventAdd.php
@@ -152,6 +152,12 @@ function ciniki_events_eventAdd(&$ciniki) {
     ciniki_core_loadMethod($ciniki, 'ciniki', 'tenants', 'private', 'updateModuleChangeDate');
     ciniki_tenants_updateModuleChangeDate($ciniki, $args['tnid'], 'ciniki', 'events');
 
+    //
+    // Update the web index if enabled
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'hookExec');
+    ciniki_core_hookExec($ciniki, $args['tnid'], 'ciniki', 'web', 'indexObject', array('object'=>'ciniki.events.event', 'object_id'=>$event_id));
+
     return array('stat'=>'ok', 'id'=>$event_id);
 }
 ?>
--- public/eventDelete.php
+++ public/eventDelete.php
@@ -250,6 +250,12 @@ function ciniki_events_eventDelete(&$ciniki) {
     ciniki_core_loadMethod($ciniki, 'ciniki', 'tenants', 'private', 'updateModuleChangeDate');
     ciniki_tenants_updateModuleChangeDate($ciniki, $args['tnid'], 'ciniki', 'events');
 
+    //
+    // Update the web index if enabled
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'hookExec');
+    ciniki_core_hookExec($ciniki, $args['tnid'], 'ciniki', 'web', 'indexObject', array('object'=>'ciniki.events.event', 'object_id'=>$args['event_id']));
+
     return array('stat'=>'ok');
 }
 ?>
